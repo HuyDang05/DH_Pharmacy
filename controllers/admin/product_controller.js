ce80aa42d4a8955adeffefa01fcc458891c38d66
HuyDang05 <quanghuy23052005@gmail.com>
2025-03-03 17:41:39 +0700

    Implement for change many item status in Products for admin

--- controllers/admin/product_controller.js
+++ controllers/admin/product_controller.js
@@ -52,8 +52,12 @@ module.exports.changeStatus = async (req, res) => {
     const id = req.params.id;
 
     await Product.updateOne({_id: id}, {status: status});
-
-
-
     res.redirect("back");
+}
+
+// [PATCH] /admin/products/change-multi
+module.exports.changeMulti = async (req, res) => {
+    
+   console.log(req.body);
+   res.send("OK");
 }
