cda87aa3bde3d2c1d6581b40c7fd450da7d29ed4
HuyDang05 <quanghuy23052005@gmail.com>
2025-03-13 21:52:14 +0700

    Notification for delete Products in Admin

--- controllers/admin/product_controller.js
+++ controllers/admin/product_controller.js
@@ -81,6 +81,7 @@ module.exports.changeMulti = async (req, res) => {
           deletedAt: new Date()
         }
       );
+      req.flash("success", `Đã xóa thành công ${ids.length} sản phẩm`);
       break;
     case "change-position":
       for (const item of ids) {
@@ -114,5 +115,6 @@ module.exports.deleteItem = async (req, res) => {
     deleted: true,
     deletedAt: new Date()
   });
+  req.flash("success", `Đã xóa thành công sản phẩm`);
   res.redirect("back");
 }
