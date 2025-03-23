e31e8ad1358a16be2dd84c9dcea0266ffbafbc96
HuyDang05 <quanghuy23052005@gmail.com>
2025-03-23 22:31:45 +0700

    Validate value for title Products in Admin

--- routes/admin/product.route.js
+++ routes/admin/product.route.js
@@ -5,6 +5,8 @@ const storageMulter = require("../../helpers/storageMulter");
 const upload = multer({ storage: storageMulter()});
 
 const controller = require("../../controllers/admin/product_controller")
+const validate = require("../../validates/admin/product.validate")
+
 
 
 router.get("/", controller.index);
@@ -13,6 +15,10 @@ router.patch("/change-status/:status/:id", controller.changeStatus);
 router.patch("/change-multi", controller.changeMulti);
 router.delete("/delete/:id", controller.deleteItem);
 router.get("/create", controller.create);
-router.post("/create", upload.single("thumbnail"), controller.createPost);
+router.post(
+    "/create", 
+    upload.single("thumbnail"),
+    validate.createPost, 
+    controller.createPost);
 
 module.exports = router;
