7ad3f1354d2c9f0f29ad5ef21f5425df1dc4a21b
HuyDang05 <quanghuy23052005@gmail.com>
2025-03-27 21:42:10 +0700

    Make product details page in Admin

--- controllers/admin/product_controller.js
+++ controllers/admin/product_controller.js
@@ -196,3 +196,25 @@ module.exports.editPatch = async (req, res) => {
   res.redirect("back");
   
 };
+
+// [GET] /admin/products/detail/:id
+module.exports.detail = async (req, res) => {
+  try {
+    const find = {
+      deleted: false,
+      _id: req.params.id
+    };
+
+    const product = await Product.findOne(find);
+
+    res.render("admin/pages/products/detail", {
+
+        pageTitle : product.title,
+        product: product
+          
+    });
+  } catch (error) {
+    req.flash("error", `Không tồn tại sản phẩm`);
+    res.redirect(`${systemConfig.prefixAdmin}/products`);
+  }
+};
