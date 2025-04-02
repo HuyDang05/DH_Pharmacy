78b95d02ddaf4f9d68d41f4b6761223e139d4fc9
HuyDang05 <quanghuy23052005@gmail.com>
2025-04-02 14:41:33 +0700

    Upload Static file to Cloud

--- routes/admin/product.route.js
+++ routes/admin/product.route.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const multer = require("multer");
+
 const router = express.Router();
-const storageMulter = require("../../helpers/storageMulter");
-const upload = multer({ storage: storageMulter()});
+
+
+
+const upload = multer();
+
 
 const controller = require("../../controllers/admin/product_controller")
 const validate = require("../../validates/admin/product.validate")
 
-
+const uploadCloud = require("../../middlewares/admin/uploadCloud.middleware");
 
 router.get("/", controller.index);
 
@@ -16,18 +20,19 @@ router.patch("/change-multi", controller.changeMulti);
 router.delete("/delete/:id", controller.deleteItem);
 router.get("/create", controller.create);
 router.post(
-    "/create", 
-    upload.single("thumbnail"),
-    validate.createPost, 
-    controller.createPost
+  "/create",
+  upload.single("thumbnail"),
+  uploadCloud.upload,
+  validate.createPost,
+  controller.createPost
 );
 
 router.get("/edit/:id", controller.edit);
 router.patch(
-    "/edit/:id", 
-    upload.single("thumbnail"),
-    validate.createPost, 
-    controller.editPatch
+  "/edit/:id",
+  upload.single("thumbnail"),
+  validate.createPost,
+  controller.editPatch
 );
 
 router.get("/detail/:id", controller.detail);
