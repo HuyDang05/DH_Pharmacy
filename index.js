ff7df248d81e50fccd7e1589bacdefd8f6819984
HuyDang05 <quanghuy23052005@gmail.com>
2025-04-05 11:14:19 +0700

    Add Text Editor  - TinyMCE

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const methodOverride = require("method-override");
 const bodyParser = require('body-parser');
 const cookieParser = require("cookie-parser");
@@ -15,7 +16,7 @@ const route = require("./routes/client/index.route");
 database.connect();
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT;  
 
 app.use(methodOverride("_method"));
 
@@ -30,6 +31,10 @@ app.use(cookieParser('xsw2wsx'));
 app.use(session({ cookie: { maxAge: 60000 }}));
 app.use(flash());
 // End Flash
+
+// TinyMCE
+app.use('/tinymce', express.static(path.join(__dirname, 'node_modules', 'tinymce')));
+// End Tiny MCE
 
 //App Locals Variables
 app.locals.prefixAdmin = systemConfig.prefixAdmin;
