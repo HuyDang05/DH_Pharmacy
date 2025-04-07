a3353979aa541afdaa49285f27e2d9939e1eec9a
HuyDang05 <quanghuy23052005@gmail.com>
2025-04-07 22:27:25 +0700

    Create Products Category in Admin (2)

--- controllers/admin/product-category_controller.js
+++ controllers/admin/product-category_controller.js
@@ -1,8 +1,35 @@
+const ProductCategory = require("../../models/product-category.model")
+const systemConfig = require("../../config/system")
+
+
 // [GET] /admin/products-category
 module.exports.index = async (req, res) => {
-  
-  res.render("admin/pages/products-category/index", {
+    res.render("admin/pages/products-category/index", {
       pageTitle : "Danh sách sản phẩm",
 
   });
+}
+
+// [GET] /admin/products-category/create
+module.exports.create = async (req, res) => {
+  res.render("admin/pages/products-category/create", {
+      pageTitle : "Tạo danh mục sản phẩm",
+
+  });
+}
+
+// [POST] /admin/products-category/create
+module.exports.createPost = async (req, res) => {
+  if ( req.body.position == "") {
+    const count = await ProductCategory.countDocuments();
+    req.body.position = count + 1;
+  } else {
+    req.body.position = parseInt(req.body.position);
+  }
+
+  const record = new ProductCategory(req.body);
+  await record.save();
+  
+  res.redirect(`${systemConfig.prefixAdmin}/products-category`);
+  res.send("OK");
 }
