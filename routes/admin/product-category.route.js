56d31cbbc364e2f019c61e0c52910fc93b5fcffd
HuyDang05 <quanghuy23052005@gmail.com>
2025-04-15 22:53:49 +0700

    Implement edit product category

--- routes/admin/product-category.route.js
+++ routes/admin/product-category.route.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const upload = multer();
 
 const controller = require("../../controllers/admin/product-category_controller");
-//const validate = require("../../validates/admin/product-category.validate")
+const validate = require("../../validates/admin/product-category.validate")
 
 const uploadCloud = require("../../middlewares/admin/uploadCloud.middleware");
 
@@ -16,9 +16,19 @@ router.post(
   "/create",
   upload.single("thumbnail"),
   uploadCloud.upload,
- // validate.createPost,
+  validate.createPost,
   controller.createPost
 );
+
+router.get("/edit/:id", controller.edit);
+
+router.patch("/edit/:id",
+  upload.single("thumbnail"),
+  uploadCloud.upload,
+  validate.createPost, 
+  controller.editPatch
+);
+
 
 
 module.exports = router;
