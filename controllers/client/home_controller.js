d18e104f93f1c6b38da0b0488163f49fc1a57388
HuyDang05 <quanghuy23052005@gmail.com>
2025-06-03 20:30:44 +0700

    Display HOT products in client

--- controllers/client/home_controller.js
+++ controllers/client/home_controller.js
@@ -1,8 +1,18 @@
-
+const Product = require("../../models/product.model");
+const productsHelper = require("../../helpers/products")
 // [GET] 
 module.exports.index = async (req, res) => {
-    
-    res.render("client/pages/home/index", {
-        pageTitle : "Trang chủ",
-    });
+  //Lấy ra sản phẩm nổi bật
+  const productsFeatured = await Product.find({
+    featured : "1",
+    deleted : false,
+    status: "active"
+  })
+
+  const newProducts = productsHelper.priceNewProducts(productsFeatured);
+
+  res.render("client/pages/home/index", {
+      pageTitle : "Trang chủ",
+      productsFeatured : newProducts
+  });
 }
