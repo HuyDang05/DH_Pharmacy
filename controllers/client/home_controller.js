db280e84a44100011d39a75398e7aaff20157707
HuyDang05 <quanghuy23052005@gmail.com>
2025-06-03 20:41:16 +0700

    Display new products in client

--- controllers/client/home_controller.js
+++ controllers/client/home_controller.js
@@ -2,17 +2,25 @@ const Product = require("../../models/product.model");
 const productsHelper = require("../../helpers/products")
 // [GET] 
 module.exports.index = async (req, res) => {
-  //Lấy ra sản phẩm nổi bật
+  // Lấy ra sản phẩm nổi bật
   const productsFeatured = await Product.find({
     featured : "1",
     deleted : false,
     status: "active"
   })
 
-  const newProducts = productsHelper.priceNewProducts(productsFeatured);
+  const newProductsFeatured = productsHelper.priceNewProducts(productsFeatured);
 
+  // Hiển thị danh sách sản phẩm mới nhất
+  const productsNew = await Product.find({
+    deleted: false,
+    status: "active"
+  }).sort({position: "desc"}).limit(6)
+
+  const newProductsNew = productsHelper.priceNewProducts(productsNew);
   res.render("client/pages/home/index", {
       pageTitle : "Trang chủ",
-      productsFeatured : newProducts
+      productsFeatured : newProductsFeatured,
+      productsNew: newProductsNew
   });
 }
