582e30d7656b902287186c25b6fc7703fe410aa5
HuyDang05 <quanghuy23052005@gmail.com>
2025-06-05 20:42:48 +0700

    Implement search by product title Page

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,10 +1,12 @@
 const homeRoutes = require("./home.route");
 const productRoutes = require("./product.route");
 const categoryMiddleware = require("../../middlewares/client/category.middleware");
+const searchRoutes = require("./search.route");
+
 
 module.exports = (app) => {
     app.use(categoryMiddleware.category);
     app.use("/", homeRoutes);
-    
+    app.use("/search", searchRoutes);
     app.use("/products", productRoutes);
 }
