118b617b712ad2b93f8c202bfc77612ebfd1670a
HuyDang05 <quanghuy23052005@gmail.com>
2025-06-12 15:32:19 +0700

    Delete products in cart

--- routes/client/cart.route.js
+++ routes/client/cart.route.js
@@ -5,5 +5,7 @@ const controller = require("../../controllers/client/cart_controller")
 
 router.get("/", controller.index);
 router.post("/add/:productId", controller.addPost);
+router.post("/add/:productId", controller.addPost);
+router.get("/delete/:productId", controller.delete);
 
 module.exports = router;
