5eaa16d1af7b46360fbecc9443cda65f1020ce35
HuyDang05 <quanghuy23052005@gmail.com>
2025-06-12 17:25:54 +0700

    Implement UI for order page in Client

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -4,6 +4,8 @@ const categoryMiddleware = require("../../middlewares/client/category.middleware
 const cartMiddleware = require("../../middlewares/client/cart.middleware");
 const searchRoutes = require("./search.route");
 const cartRoutes = require("./cart.route");
+const checkoutRoutes = require("./checkout.route");
+
 
 
 
@@ -14,5 +16,7 @@ module.exports = (app) => {
     app.use("/search", searchRoutes);
     app.use("/products", productRoutes);
     app.use("/cart", cartRoutes);
+    app.use("/checkout", checkoutRoutes);
+
 
 }
