ffe1dde73390e4043ef858e746f2de411a3dd61e
HuyDang05 <quanghuy23052005@gmail.com>
2025-06-14 16:39:55 +0700

    Implement Login Page in Client

--- validates/client/user.validate.js
+++ validates/client/user.validate.js
@@ -17,7 +17,21 @@ module.exports.registerPost = (req, res, next) => {
         return;
     }
 
+    next();
+}
 
+module.exports.loginPost = (req, res, next) => {
+    if (!req.body.email) {
+        req.flash("error", "Email không được để trống!");
+        res.redirect("back");
+        return;
+    }
+
+    if (!req.body.password) {
+        req.flash("error", "Mật khẩu không được để trống!");
+        res.redirect("back");
+        return;
+    }
 
     next();
 }
