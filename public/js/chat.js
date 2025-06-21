bc4525ce5090f9a71fb65c0941cbf723beecce74
HuyDang05 <quanghuy23052005@gmail.com>
2025-06-21 16:18:37 +0700

    Typing Chat effect in Client

--- public/js/chat.js
+++ public/js/chat.js
@@ -9,6 +9,7 @@ import * as Popper from 'https://cdn.jsdelivr.net/npm/@popperjs/core@^2/dist/esm
       if(content) {
         socket.emit("CLIENT_SEND_MESSAGE", content);
         e.target.elements.content.value = "";
+        socket.emit("CLIENT_SEND_TYPING", "hidden");
       }
     })
   }
@@ -18,6 +19,7 @@ import * as Popper from 'https://cdn.jsdelivr.net/npm/@popperjs/core@^2/dist/esm
 socket.on("SERVER_RETURN_MESSAGE", (data) => {
   const myId = document.querySelector("[my-id]").getAttribute("my-id");
   const body = document.querySelector(".chat .inner-body");
+  const boxTyping = document.querySelector(".inner-list-typing");
 
   const div = document.createElement("div");
 
@@ -35,7 +37,7 @@ socket.on("SERVER_RETURN_MESSAGE", (data) => {
     <div class="inner-content">${data.content}</div>
   `;
 
-  body.appendChild(div);
+  body.insertBefore(div, boxTyping);
 
   body.scrollTop = body.scrollHeight;
 })
@@ -49,6 +51,20 @@ if (bodyChat) {
 }
 
 // End Scroll Chat to Bottom 
+
+//End Show Typing
+var timeOut;
+const showTyping = () => {
+  socket.emit("CLIENT_SEND_TYPING", "show");
+
+  clearTimeout(timeOut);
+
+  timeOut = setTimeout(() => {
+    socket.emit("CLIENT_SEND_TYPING", "hidden");
+  }, 3000);
+}
+// End Show Typing
+
 
 // emoji-picker
 // Show Popup
@@ -71,6 +87,50 @@ if (emojiPicker) {
   emojiPicker.addEventListener("emoji-click", (event) => {
     const icon = event.detail.unicode;
     inputChat.value = inputChat.value + icon;
+
+    const end  = inputChat.value.length;
+    inputChat.setSelectionRange(end, end);
+    inputChat.focus();
+
+    showTyping();
+  });
+
+  inputChat.addEventListener("keyup", () => {
+    showTyping();
   })
 }
 // End emoji-picker
+
+// SERVER_RETURN_TYPING
+const elementListTyping = document.querySelector(".chat .inner-list-typing");
+if(elementListTyping) {
+  socket.on("SERVER_RETURN_TYPING", (data) => {
+    if(data.type == "show") {
+      const existTyping = elementListTyping.querySelector(`[user-id="${data.userId}"]`);
+
+      if (!existTyping) {
+        const bodyChat = document.querySelector(".chat .inner-body");
+        const boxTyping = document.createElement("div");
+        boxTyping.classList.add("box-typing");
+        boxTyping.setAttribute("user-id", data.userId);
+
+        boxTyping.innerHTML = `
+          <div class="inner-name">${data.fullName}</div>
+          <div class="inner-dots">
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        `;
+
+        elementListTyping.appendChild(boxTyping);
+        bodyChat.scrollTop = bodyChat.scrollHeight;
+      }
+    } else {
+      const boxTypingRemove = elementListTyping.querySelector(`[user-id="${data.userId}"]`);
+      if (boxTypingRemove) {
+        elementListTyping.removeChild(boxTypingRemove);
+      }
+    }
+  })
+}
