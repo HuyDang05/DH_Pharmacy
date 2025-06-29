4bb2cea38103576b13c87ad239119090bbaa764a
HuyDang05 <quanghuy23052005@gmail.com>
2025-06-29 23:10:17 +0700

    Display online status realtime

--- controllers/client/user_controller.js
+++ controllers/client/user_controller.js
@@ -80,6 +80,11 @@ module.exports.loginPost = async (req, res) => {
     statusOnline: "online"
   });
 
+  _io.once('connection', (socket) => {
+    socket.broadcast.emit("SERVER_RETURN_USER_ONLINE", user.id);
+  });
+
+  // Lưu user_id vào collection cart
   await Cart.updateOne({
     _id: req.cookies.cartId
   }, {
@@ -95,7 +100,12 @@ module.exports.logout = async (req, res) => {
 
   await User.updateOne({ _id: res.locals.user.id}, {
     statusOnline: "offline"
-  })
+  });
+
+  _io.once('connection', (socket) => {
+    socket.broadcast.emit("SERVER_RETURN_USER_OFFLINE", res.locals.user.id);
+  });
+
   res.clearCookie("tokenUser");
   res.redirect("/");
 } 
